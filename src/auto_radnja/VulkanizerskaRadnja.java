1398b3be4416a99ecb9f45bf71301b35e16deb1a
goran9999 <gutovicgoran@gmail.com>
2022-03-20 20:49:43 +0100

    Napisan unit test za interfejs Radnja i implementiran u test klasi VulkanizerskaRadnjaTest

--- src/auto_radnja/VulkanizerskaRadnja.java
+++ src/auto_radnja/VulkanizerskaRadnja.java
@@ -22,16 +22,17 @@ public class VulkanizerskaRadnja implements Radnja {
 			throw new NullPointerException("Guma ne sme biti null");
 		if (gume.contains(a))
 			throw new RuntimeException("Guma vec postoji");
-		((LinkedList<AutoGuma>) gume).addFirst(a);
+		 gume.add(a);
 	}
 
+	//U ovoj metodi je bila greska.Dodato je .getMarkaModel() u if naredbi kako bi se uporedjivali stringovi a ne objekat sa stringom.
 	@Override
 	public List<AutoGuma> pronadjiGumu(String markaModel) {
 		if (markaModel == null)
 			return null;
 		List<AutoGuma> novaLista = new LinkedList<AutoGuma>();
 		for (int i = 0; i < gume.size(); i++)
-			if (gume.get(i).equals(markaModel))
+			if (gume.get(i).getMarkaModel().equals(markaModel))
 				novaLista.add(gume.get(i));
 		return novaLista;
 	}
---
+++ test/auto_radnja/RadnjaTest.java
@@ -0,0 +1,83 @@
+package auto_radnja;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+import java.util.List;
+
+
+import org.junit.jupiter.api.Test;
+
+import gume.AutoGuma;
+
+abstract class RadnjaTest {
+
+	protected Radnja radnja;
+
+	@Test
+	void testDodajGumu() {
+		AutoGuma autoGuma = new AutoGuma("Bridgestone 55", 15, 145, 85);
+		radnja.dodajGumu(autoGuma);
+		assertEquals(radnja.vratiSveGume().size(), 1);
+		assertEquals(radnja.vratiSveGume().get(0), autoGuma);
+	}
+
+	@Test
+	void testDodajGumuNull() {
+	Exception e=assertThrows(java.lang.NullPointerException.class, () -> radnja.dodajGumu(null));
+	assertEquals("Guma ne sme biti null", e.getMessage());
+	}
+
+	@Test
+	void testDodajPostojecuGumu() {
+		AutoGuma autoGuma = new AutoGuma("Bridgestone 55", 15, 145, 85);
+		radnja.dodajGumu(autoGuma);
+		AutoGuma autoGuma2 = new AutoGuma("Bridgestone 55", 15, 145, 85);
+		Exception e=assertThrows(java.lang.RuntimeException.class, () -> radnja.dodajGumu(autoGuma2));
+		assertEquals("Guma vec postoji", e.getMessage());
+	}
+
+	@Test
+	void testPronadjiGumuNull() {
+		
+		assertEquals(null, radnja.pronadjiGumu(null));
+		
+	}
+	
+	@Test
+	void testNePostojiZadataGuma() {
+		AutoGuma autoGuma = new AutoGuma("Bridgestone 55", 15, 145, 85);
+		radnja.dodajGumu(autoGuma);
+		assertEquals(0, radnja.pronadjiGumu("Dunlop 123CV").size());
+	}
+	
+	@Test
+	void testPronadjiViseGumaIsteMarkeIModela() {
+		AutoGuma autoGuma = new AutoGuma("Bridgestone 55", 15, 145, 85);
+		AutoGuma autoGuma2 = new AutoGuma("Bridgestone 55", 13, 139, 78);
+		radnja.dodajGumu(autoGuma);
+		radnja.dodajGumu(autoGuma2);
+		List<AutoGuma> gume=radnja.pronadjiGumu("Bridgestone 55");
+		assertEquals(2, gume.size());
+		assertEquals(gume.get(0).getMarkaModel(), gume.get(1).getMarkaModel());
+		assertNotEquals(gume.get(0).getSirina(), gume.get(1).getSirina());
+		assertNotEquals(gume.get(0).getVisina(), gume.get(1).getVisina());
+		assertNotEquals(gume.get(0).getPrecnik(), gume.get(1).getPrecnik());
+		
+	}
+
+	@Test
+	void testVratiSveGume() {
+		AutoGuma autoGuma = new AutoGuma("Bridgestone 55", 15, 145, 85);
+		AutoGuma autoGuma2 = new AutoGuma("Bridgestone 55", 13, 139, 78);
+		radnja.dodajGumu(autoGuma);
+		radnja.dodajGumu(autoGuma2);
+		assertEquals(2, radnja.vratiSveGume().size());
+	}
+	
+	@Test
+	void vratiSveGumePraznaLista() {
+		assertEquals(0,radnja.vratiSveGume().size());
+	}
+
+}
